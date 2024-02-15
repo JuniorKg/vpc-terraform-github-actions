fd9815680383e1d4984d896dc8b943ce55e3faf6
dana <jumgal91@gmail.com>
2024-02-15 18:49:36 -0500

    Adding Terraform Files for CICD Project with Github

---
+++ Terraform-VPC/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.36.0"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-remote-backend-junior-s3"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+provider "aws" {
+  # Configuration options
+}
